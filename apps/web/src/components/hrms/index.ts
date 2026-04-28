546dbf6af2384c9d9f01b8741e1ff6247860b3dd
Provintell Cyberlab <cyberlab@provintell.com>
2026-04-29 01:32:48 +0800

    chore(ui): barrel-export hrms composed components

---
+++ apps/web/src/components/hrms/index.ts
@@ -0,0 +1,29 @@
+export { ApprovalActionBar } from "./ApprovalActionBar";
+export type { ApprovalActionBarProps } from "./ApprovalActionBar";
+export { AttendanceLogRow } from "./AttendanceLogRow";
+export type { AttendanceLogRowProps } from "./AttendanceLogRow";
+export { ClockInOutWidget } from "./ClockInOutWidget";
+export type { ClockInOutWidgetProps, ClockState } from "./ClockInOutWidget";
+export { DataTable } from "./DataTable";
+export type { Column, DataTableProps } from "./DataTable";
+export { DetailPanel } from "./DetailPanel";
+export type { DetailPanelProps } from "./DetailPanel";
+export { DonutChart } from "./DonutChart";
+export type { DonutChartProps, DonutSegment } from "./DonutChart";
+export { EmployeeCard } from "./EmployeeCard";
+export type { EmployeeCardProps } from "./EmployeeCard";
+export { EmptyState } from "./EmptyState";
+export type { EmptyStateProps } from "./EmptyState";
+export { FileUploader } from "./FileUploader";
+export type { FileUploaderProps, PresignedUpload } from "./FileUploader";
+export { KpiTile } from "./KpiTile";
+export type { KpiTileProps } from "./KpiTile";
+export { NotificationCard } from "./NotificationCard";
+export type {
+	NotificationCardProps,
+	NotificationDTO,
+} from "./NotificationCard";
+export { ProgressBar } from "./ProgressBar";
+export type { ProgressBarProps } from "./ProgressBar";
+export { StatusPill } from "./StatusPill";
+export type { StatusPillProps } from "./StatusPill";
